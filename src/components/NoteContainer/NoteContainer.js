78eb473644b6f63ac538ac06018c76a24edc8e89
Pranjal <happybarnwal007@gmail.com>
2023-03-16 18:56:47 +0530

    still adding delete functionality

--- src/components/NoteContainer/NoteContainer.js
+++ src/components/NoteContainer/NoteContainer.js
@@ -1,119 +1,27 @@
-import React from 'react'
-import "./NoteContainer.css"
-import Note from '../Note/Note'
-const NoteContainer = () => {
+import React from "react";
+import "./NoteContainer.css";
+import Note from "../Note/Note";
+const NoteContainer = (props) => {
+
+  const notes=props.notes;
   return (
-    <div className='note-container'>
+    <div className="note-container">
+      
       <h2>Notes</h2>
+      
       <div className="note-container_notes custom-scroll">
-      <Note note={{
-        text:"hello happy how are you",
-        time:"2:11 AM",
-        color:"cyan"
-      }}/>
-      <Note note={{
-        text:"hello happy how are you",
-        time:"2:11 AM",
-        color:"cyan"
-      }}/>
-      <Note note={{
-        text:"hello happy how are you",
-        time:"2:11 AM",
-        color:"cyan"
-      }}/>
-      <Note note={{
-        text:"hello happy how are you",
-        time:"2:11 AM",
-        color:"cyan"
-      }}/>
-      <Note note={{
-        text:"hello happy how are you",
-        time:"2:11 AM",
-        color:"cyan"
-      }}/>
-      <Note note={{
-        text:"hello happy how are you",
-        time:"2:11 AM",
-        color:"cyan"
-      }}/>
-      <Note note={{
-        text:"hello happy how are you",
-        time:"2:11 AM",
-        color:"cyan"
-      }}/>
-      <Note note={{
-        text:"hello happy how are you",
-        time:"2:11 AM",
-        color:"cyan"
-      }}/>
-      <Note note={{
-        text:"hello happy how are you",
-        time:"2:11 AM",
-        color:"cyan"
-      }}/>
-      <Note note={{
-        text:"hello happy how are you",
-        time:"2:11 AM",
-        color:"cyan"
-      }}/>
-      <Note note={{
-        text:"hello happy how are you",
-        time:"2:11 AM",
-        color:"cyan"
-      }}/>
-      <Note note={{
-        text:"hello happy how are you",
-        time:"2:11 AM",
-        color:"cyan"
-      }}/>
-      <Note note={{
-        text:"hello happy how are you",
-        time:"2:11 AM",
-        color:"cyan"
-      }}/>
-      <Note note={{
-        text:"hello happy how are you",
-        time:"2:11 AM",
-        color:"cyan"
-      }}/>
-      <Note note={{
-        text:"hello happy how are you",
-        time:"2:11 AM",
-        color:"cyan"
-      }}/>
-      <Note note={{
-        text:"hello happy how are you",
-        time:"2:11 AM",
-        color:"cyan"
-      }}/>
-      <Note note={{
-        text:"hello happy how are you",
-        time:"2:11 AM",
-        color:"cyan"
-      }}/>
-      <Note note={{
-        text:"hello happy how are you",
-        time:"2:11 AM",
-        color:"cyan"
-      }}/>
-      <Note note={{
-        text:"hello happy how are you",
-        time:"2:11 AM",
-        color:"cyan"
-      }}/>
-      <Note note={{
-        text:"hello happy how are you",
-        time:"2:11 AM",
-        color:"cyan"
-      }}/>
-      <Note note={{
-        text:"hello happy how are you",
-        time:"2:11 AM",
-        color:"cyan"
-      }}/>
+        {
+        notes.length > 0 ? (
+          notes.map((item, index) => <Note key={item.id} note={item} 
+          deleteNote={props.deleteNote}
+          />)
+        ) : (
+          <h2>No notes present</h2>
+        )}
+      
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default NoteContainer
+export default NoteContainer;
